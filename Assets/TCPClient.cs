abb7cfaa2f16e4304e8277e8c8a745b7407728b6
zhoulk <849693228@qq.com>
2018-12-10 21:25:15 +0800

    Initial commit

---
+++ Assets/TCPClient.cs
@@ -0,0 +1,52 @@
+﻿//===================================================
+//作    者：#AuthorName# 
+//创建时间：#CreateTime#
+//备    注：
+//===================================================
+
+using Google.Protobuf;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using UnityEngine;
+
+public class TCPClient : MonoBehaviour {
+
+
+    private Socket tcpSocket;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    public void connect()
+    {
+        //创建socket
+        tcpSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+        //连接服务器
+        tcpSocket.Connect(IPAddress.Parse("127.0.0.1"), 12321);
+        Debug.Log("连接服务器");
+
+        ////接收消息
+        //byte[] bt = new byte[1024];
+        //int messgeLength = tcpSocket.Receive(bt);
+        ////Debug.Log(ASCIIEncoding.UTF8.GetString(bt));
+
+        SearchRequest search = new SearchRequest();
+        search.ResultPerPage = 100;
+        search.PageNumber = 100;
+        byte[] temp = search.ToByteArray();
+
+        //发送消息
+        tcpSocket.Send(temp);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Assets/main.cs
@@ -0,0 +1,32 @@
+﻿//===================================================
+//作    者：#AuthorName# 
+//创建时间：#CreateTime#
+//备    注：
+//===================================================
+
+using Google.Protobuf;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SearchRequest search = new SearchRequest();
+        search.ResultPerPage = 100;
+        search.PageNumber = 100;
+        byte[] temp = search.ToByteArray();
+
+        Debug.Log(search.ToString());
+        Debug.Log(search.ToByteArray().Length);
+
+        SearchRequest search2 = SearchRequest.Parser.ParseFrom(temp);
+        Debug.Log(search2.PageNumber);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
